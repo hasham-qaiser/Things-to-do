732cbbfcd886f445e5ee3dd9999414e1f31a970b
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2022-12-12 21:15:46 -0800

    updated html, js, svelte

---
+++ src/components/dashboard/Todoform.svelte
@@ -0,0 +1,34 @@
+<div>
+	<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+		This is your todo list
+	</h2>
+</div>
+<form class="mt-8 space-y-6" action="#" method="POST">
+	<input type="hidden" name="remember" value="true" />
+	<div class="-space-y-px rounded-md shadow-sm">
+		<div>
+			<label for="todo-input" class="sr-only">Email address</label>
+			<input
+				id="todo-input"
+				name="todo-input"
+				type="text"
+				autocomplete="email"
+				required
+				class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+				placeholder="Enter an activity"
+			/>
+		</div>
+	</div>
+
+	<div>
+		<button
+			type="submit"
+			class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+		>
+			<span class="absolute inset-y-0 left-0 flex items-center pl-3">
+				<!-- Heroicon name: mini/lock-closed -->
+			</span>
+			Add to list
+		</button>
+	</div>
+</form>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Hero from '../components/marketing/hero.svelte';
+	import Features from '../components/marketing/features.svelte';
+</script>
+
+<Hero />
+<Features />
