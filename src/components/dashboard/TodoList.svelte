726bca7cf821ac84225bfc94fe45f0fdf78a3cfb
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2022-12-13 13:49:13 -0800

    fixed bug for list but still has issues

--- src/components/dashboard/TodoList.svelte
+++ src/components/dashboard/TodoList.svelte
@@ -2,13 +2,14 @@
 	import { text } from 'svelte/internal';
 
 	export let todos;
+	export let remove;
 </script>
 
 <div class="rounded-lg shadow-lg p-8 flex flex-col gap-4">
 	{#each todos as item, index}
 		<div class="rounded-md p-3 justify-between flex flex-row border-2 border-gray-800">
 			<div class="font-medium text-sm">{item.text}</div>
-			<button class="font-light text-sm">remove</button>
+			<button on:click={() => remove(index)} class="font-light text-sm">remove</button>
 		</div>
 	{/each}
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -16,14 +16,55 @@
 	];
 	function addToList() {
 		todoList = [{ text: todoInput, status: true }, ...todoList];
+		todoInput = '';
+	}
+
+	function removeFromList(i) {
+		todoList.splice(i, 1);
+		todoList = todoList;
 	}
 </script>
 
 <div class="h-screen">
 	<div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 		<div class="w-full max-w-md space-y-16">
-			<Todoform {todoInput} add={addToList} />
-			<TodoList todos={todoList} />
+			<div>
+				<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+					This is your todo list
+				</h2>
+			</div>
+			<form class="mt-8 space-y-6" on:submit={addToList}>
+				<input type="hidden" name="remember" value="true" />
+				<div class="-space-y-px rounded-md shadow-sm">
+					<div>
+						<label for="todo-input" class="sr-only">Email address</label>
+						<input
+							bind:value={todoInput}
+							id="todo-input"
+							name="todo-input"
+							type="text"
+							autocomplete="email"
+							required
+							class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+							placeholder="Enter an activity"
+						/>
+					</div>
+				</div>
+
+				<div>
+					<button
+						type="submit"
+						class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					>
+						<span class="absolute inset-y-0 left-0 flex items-center pl-3">
+							<!-- Heroicon name: mini/lock-closed -->
+						</span>
+						Add to list
+					</button>
+				</div>
+			</form>
+
+			<TodoList todos={todoList} remove={removeFromList} />
 		</div>
 	</div>
 </div>
